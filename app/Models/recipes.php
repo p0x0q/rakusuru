1280216b7c63fff547eb692c6b8436c44aad4de2
p0x0q <info.allegation@gmail.com>
2021-06-30 21:44:52 +0900

    ok

--- app/Models/recipes.php
+++ app/Models/recipes.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class recipes extends Model
 {
     use HasFactory;
+    protected $table = 'recipes';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    protected $guarded = [
+        'id'
+    ];
 }
